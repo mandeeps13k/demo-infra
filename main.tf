7efa457c04a449ed7e0162119ece138eecd245d6
Mandeep Singh Kapoor <mandeeps13k@gmail.com>
2023-12-05 10:21:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,13 @@ resource "aws_security_group" "ec2_instance_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  
   }
+
+  egress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [ aws_security_group.rds_db_security_group.id ]
+  }
 }
 
 
